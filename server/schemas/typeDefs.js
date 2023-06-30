87e6b08780a36efc59e03460ec0bfe38ce867b9e
Danny Tran <danieltran2016@gmail.com>
2023-06-29 21:08:37 -0500

    branch protetctions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,16 +7,16 @@ const typeDefs = gql`
     email: String
     bookCount: Int
     savedBooks: [Book]!
-  }
+  } 
 
-  type Book {
-    bookId: String
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
+  type Restaurant { 
+    bookId: String 
+    authors: [String] 
+    description: String 
+    title: String 
+    image: String 
+    link: String 
+  } 
 
   type Auth {
     token: ID!
