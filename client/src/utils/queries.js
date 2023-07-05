c7034c533ef41988add836f0cd0ef8be6a37541e
xybai0103 <xueyin0103@gmail.com>
2023-07-04 22:40:43 -0500

    add queries on the client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,61 @@
 import { gql } from '@apollo/client';
 
-export const GET_ME = gql`
-  query me {
-    me {
+export const GET_PLACESTOGO = gql`
+  query getPlacesToGo {
+    getPlacesToGo {
       _id
-      username
-      email
-      bookCount
-      savedBooks {
-        authors
-        bookId
-        description
-        image
-        link
-        title
-      }  
+      restaurants {
+        _id
+        address
+        name
+      }
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const GET_PLACESILIKE = gql`
+  query getPlacesILike {
+    getPlacesILike {
+      _id
+      restaurants {
+        restaurant {
+          _id
+          address
+          name
+        }
+        comment
+      }
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const GET_PLACESIDONTLIKE = gql`
+  query getPlacesIDontLike {
+    getPlacesIDontLike {
+      _id
+      restaurants {
+        restaurant {
+          _id
+          address
+          name
+        }
+        comment
+      }
+      user {
+        _id
+        email
+        username
+      }
     }
   }
 `;
