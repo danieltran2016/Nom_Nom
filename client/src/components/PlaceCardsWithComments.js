87944073f2b997a5fd01f9d9d1048547b449eac0
William Combee <william.combee@yahoo.com>
2023-07-05 20:41:56 -0500

    added delete button and handledelete

--- client/src/components/PlaceCardsWithComments.js
+++ client/src/components/PlaceCardsWithComments.js
@@ -6,20 +6,34 @@ const PlaceCardsWithComments = ({ getPlacesILike }) => {
   if (!restaurants.length) {
     return <h3>No restaurants found</h3>;
   }
+    const handleDelete = (restaurantId) => {
+    console.log('Deleting restaurant with ID:', restaurantId);
+  };
 
   return (
     <div>
       {restaurants.map((restaurant) => (
-        <div key={restaurant.restaurant._id}>
-          <h3>{restaurant.restaurant.name}</h3>
-          <p>{restaurant.restaurant.address}</p>
-          {restaurant.comment.map((comment) => (
-            <div key={comment._id} className="card mb-3">
-              <div className="card-body bg-light p-2">
-                <p>{comment.commentText}</p>
+        <div key={restaurant.restaurant._id} className="card mb-3">
+          <div className="card-header bg-primary text-light p-2">
+            <h3>{restaurant.restaurant.name}</h3>
+            <button
+              className="btn btn-danger btn-sm"
+              style={{ position: 'absolute', top: '5px', right: '5px' }}
+              onClick={() => handleDelete(restaurant.restaurant._id)}
+            >
+              X
+            </button>
+          </div>
+          <div className="card-body bg-light p-2">
+            <p>{restaurant.restaurant.address}</p>
+            {restaurant.comment.map((comment) => (
+              <div key={comment._id} className="card mb-3">
+                <div className="card-body bg-light p-2">
+                  <p>{comment.commentText}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       ))}
     </div>
@@ -27,3 +41,4 @@ const PlaceCardsWithComments = ({ getPlacesILike }) => {
 };
 
 export default PlaceCardsWithComments;
+
