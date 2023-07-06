7a08a03e34e9fbcadbe12f2ce368efb04af997f6
William Combee <william.combee@yahoo.com>
2023-07-05 19:56:26 -0500

    adding to comments page

--- client/src/components/PlaceCardsWithComments.js
+++ client/src/components/PlaceCardsWithComments.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Restaurant } from '../../../server/models';
 
-const PlaceCardsWithComments = ({ name, address, comment }) => {
-  if (!Restaurant.length) {
-    return <h3>Restaurant not found</h3>;
+const PlaceCardsWithComments = ({ name, address, comments }) => {
+  if (!comments.length) {
+    return <h3>No comments found for the restaurant</h3>;
   }
 
   return (
     <div>
       <h3>{name}</h3>
-      {thoughts &&
-        thoughts.map((thought) => (
-          <div key={thought._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {thought.thoughtAuthor} <br />
-              <span style={{ fontSize: '1rem' }}>
-                had this thought on {thought.createdAt}
-              </span>
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{thought.thoughtText}</p>
-            </div>
+      <p>{address}</p>
+      {comments.map((comment) => (
+        <div key={comment._id} className="card mb-3">
+          <h4 className="card-header bg-primary text-light p-2 m-0">
+            {comment.commentAuthor} <br />
+            <span style={{ fontSize: '1rem' }}>
+              commented on {comment.createdAt}
+            </span>
+          </h4>
+          <div className="card-body bg-light p-2">
+            <p>{comment.commentText}</p>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 };
