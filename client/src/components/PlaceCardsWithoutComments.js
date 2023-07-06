7e7d7887300b61efc3992f063888969656b7a0ad
William Combee <william.combee@yahoo.com>
2023-07-05 23:43:59 -0500

    added last delete mutation to the menu

--- client/src/components/PlaceCardsWithoutComments.js
+++ client/src/components/PlaceCardsWithoutComments.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { VscKebabVertical } from 'react-icons/fa';
-import { ADDTO_PLACESILIKE } from '../utils/mutations';
+import {
+  MOVE_RESTAURANTTOPLACESILIKE,
+  MOVE_RESTAURANTTOPLACESIDONTLIKE,
+  REMOVEFROM_PLACESTOGO
+} from '../utils/mutations';
 
 const PlaceCardsWithoutComments = ({ getPlacesToGo }) => {
   const { restaurants } = getPlacesToGo;
@@ -11,7 +15,9 @@ const PlaceCardsWithoutComments = ({ getPlacesToGo }) => {
   }
 
   const [menuVisible, setMenuVisible] = useState(false);
-  const [addToPlacesILike] = useMutation(ADDTO_PLACESILIKE);
+  const [addToPlacesILike] = useMutation(MOVE_RESTAURANTTOPLACESILIKE);
+  const [addToPlacesIDontLike] = useMutation(MOVE_RESTAURANTTOPLACESIDONTLIKE);
+  const [removeFromPlacesToGo] = useMutation(REMOVEFROM_PLACESTOGO);
 
   const handleMenuToggle = (index) => {
     setMenuVisible((prev) => (prev === index ? false : index));
@@ -34,14 +40,44 @@ const PlaceCardsWithoutComments = ({ getPlacesToGo }) => {
       });
   };
 
-  const handleAddToPlacesIDontLike = (restaurantId) => {
-    console.log('Adding to Places I Don\'t Like:', restaurantId);
-    // Implement the logic to add the restaurant to Places I Don't Like
+  const handleAddToPlacesIDontLike = (restaurant) => {
+    addToPlacesIDontLike({
+      variables: {
+        name: restaurant.name,
+        address: restaurant.address,
+      },
+      update(cache, { data: { addToPlacesIDontLike } }) {
+      },
+    })
+      .then(({ data }) => {
+        console.log(
+          'Restaurant added to Places I Don\'t Like:',
+          data.addToPlacesIDontLike
+        );
+      })
+      .catch((error) => {
+        console.error(
+          'Error adding restaurant to Places I Don\'t Like:',
+          error
+        );
+      });
   };
 
   const handleDelete = (restaurantId) => {
     console.log('Deleting restaurant:', restaurantId);
-    // Implement the logic to delete the restaurant
+    removeFromPlacesToGo({
+      variables: {
+        restaurantId: restaurantId,
+      },
+      update(cache, { data: { removeFromPlacesToGo } }) {
+      },
+    })
+      .then(({ data }) => {
+        console.log('Restaurant deleted:', data.removeFromPlacesToGo);
+      })
+      .catch((error) => {
+        console.error('Error deleting restaurant:', error);
+      });
   };
 
   return (
@@ -55,15 +91,13 @@ const PlaceCardsWithoutComments = ({ getPlacesToGo }) => {
         >
           <div className="card-header bg-primary text-light p-2">
             <h3>{restaurant.name}</h3>
-            {menuVisible === index && (
+            {menuVisible === index&& (
               <div className="menu-container">
                 <div className="menu-links">
                   <button onClick={() => handleAddToPlacesILike(restaurant)}>
                     Add to Places I Like
                   </button>
-                  <button
-                    onClick={() => handleAddToPlacesIDontLike(restaurant._id)}
-                  >
+                  <button onClick={() => handleAddToPlacesIDontLike(restaurant)}>
                     Add to Places I Don't Like
                   </button>
                   <button onClick={() => handleDelete(restaurant._id)}>
@@ -88,5 +122,6 @@ const PlaceCardsWithoutComments = ({ getPlacesToGo }) => {
 };
 
 export default PlaceCardsWithoutComments;
+
 
 
