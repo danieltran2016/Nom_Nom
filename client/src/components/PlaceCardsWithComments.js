f65f5e89592e0a0fbec7e30681b17767c37a191b
William Combee <william.combee@yahoo.com>
2023-07-05 20:23:45 -0500

    fixing PlaceCards w/ Comments

--- client/src/components/PlaceCardsWithComments.js
+++ client/src/components/PlaceCardsWithComments.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Restaurant } from '../../../server/models';
 
-const PlaceCardsWithComments = ({ name, address, comments }) => {
-  if (!comments.length) {
-    return <h3>No comments found for the restaurant</h3>;
+const PlaceCardsWithComments = ({ getPlacesILike }) => {
+  const { restaurants } = getPlacesILike;
+
+  if (!restaurants.length) {
+    return <h3>No restaurants found</h3>;
   }
 
   return (
     <div>
-      <h3>{name}</h3>
-      <p>{address}</p>
-      {comments.map((comment) => (
-        <div key={comment._id} className="card mb-3">
-          <h4 className="card-header bg-primary text-light p-2 m-0">
-            {comment.commentAuthor} <br />
-            <span style={{ fontSize: '1rem' }}>
-              commented on {comment.createdAt}
-            </span>
-          </h4>
-          <div className="card-body bg-light p-2">
-            <p>{comment.commentText}</p>
-          </div>
+      {restaurants.map((restaurant) => (
+        <div key={restaurant.restaurant._id}>
+          <h3>{restaurant.restaurant.name}</h3>
+          <p>{restaurant.restaurant.address}</p>
+          {restaurant.comment.map((comment) => (
+            <div key={comment._id} className="card mb-3">
+              <div className="card-body bg-light p-2">
+                <p>{comment.commentText}</p>
+              </div>
+            </div>
+          ))}
         </div>
       ))}
     </div>
