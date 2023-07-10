a125ff467a7ab0e0b0dd060701f5862e50214976
William Combee <william.combee@yahoo.com>
2023-07-09 20:38:56 -0500

    fixed positioning of randomizer

--- client/src/components/PlaceCardsWithComments.js
+++ client/src/components/PlaceCardsWithComments.js
@@ -43,43 +43,45 @@ const PlaceILikeCardsWithComments = ({ restaurants }) => {
   };
 
   return (
-    <div className="card-grid" style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {restaurants.map((restaurant) => (
-        <Card
-          key={restaurant.restaurant._id}
-          className="card mb-3"
-          style={{ width: '50%' }}
-        >
-          <Card.Header className="bg-warning text-dark p-2">
-            <h3>{restaurant.restaurant.name}</h3>
-            <Button
-              variant="dark"
-              className="btn-sm"
-              style={{ position: 'absolute', top: '5px', right: '5px' }}
-              onClick={() => handleDelete(restaurant.restaurant._id)}
-            >
-              <RiDeleteBin2Line />
-            </Button>
-          </Card.Header>
-          <Card.Body className="bg-light p-2">
-            <p>{restaurant.restaurant.address}</p>
-            <Card className="mb-3">
-              <Card.Body className="bg-light-emphasis p-2">
-                <p>{restaurant.comment}</p>
-              </Card.Body>
-            </Card>
-          </Card.Body>
-        </Card>
-      ))}
-      <div>
-        <h3>Random Restaurant Selector</h3>
+    <div>
+      <div className="card-grid" style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {restaurants.map((restaurant) => (
+          <Card
+            key={restaurant.restaurant._id}
+            className="card mb-3"
+            style={{ width: '50%' }}
+          >
+            <Card.Header className="bg-warning text-dark p-2">
+              <h3>{restaurant.restaurant.name}</h3>
+              <Button
+                variant="dark"
+                className="btn-sm"
+                style={{ position: 'absolute', top: '5px', right: '5px' }}
+                onClick={() => handleDelete(restaurant.restaurant._id)}
+              >
+                <RiDeleteBin2Line />
+              </Button>
+            </Card.Header>
+            <Card.Body className="bg-light p-2">
+              <p>{restaurant.restaurant.address}</p>
+              <Card className="mb-3">
+                <Card.Body className="bg-light-emphasis p-2">
+                  <p>{restaurant.comment}</p>
+                </Card.Body>
+              </Card>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+      <div className="randomizer-container" style={{ width: '50%' }}>
+        <h3>Let Fate Decide</h3>
         <Button variant="secondary" onClick={randomizeRestaurant}>
           Select Random Restaurant
         </Button>
         {selectedRestaurant && (
           <Card className="card mt-3">
             <Card.Body>
-              <h5>Selected Restaurant:</h5>
+              <h5>You Will Nom Nom at:</h5>
               <p>{selectedRestaurant}</p>
             </Card.Body>
           </Card>
