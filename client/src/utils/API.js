e34538be992eb081c63b655d1877aabdb2a91b34
xybai0103 <xueyin0103@gmail.com>
2023-07-12 17:00:31 -0500

    delete the unused API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,3 @@
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-// export const searchGoogleBooks = (query) => {
-//   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-// };
-
 // export const searchGooglePlaces = (location, search) => {
 //   return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json
 //   ?location=${location}121.2121%2c12.21
