a51341eedc2f187642eb2867ba6c4fff323f0f77
William Combee <william.combee@yahoo.com>
2023-07-12 17:22:35 -0500

    added icons to side nav

--- client/src/components/SideNavbar.js
+++ client/src/components/SideNavbar.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Nav, Container} from 'react-bootstrap';
+import { Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { PlacesToGoForm, PlacesILikeForm, PlacesIDontLikeForm } from './SideAddForm';
+import { BiHappyHeartEyes, BiSolidDislike } from 'react-icons/bi';
+import { FaHeart } from 'react-icons/fa';
 
 const SideNavbar = () => {
   return (
     <div>
       <Container className="pb-5 pt-3">
-        <Nav.Link as={Link} to={`/PlacesToGo`} className="text-warning">
-          Places To Go
+        <Nav.Link as={Link} to="/PlacesToGo" className="text-warning">
+          Places To Go {''}
+          <FaHeart size={25} style={{ display: 'inline-block', marginBottom: '5px' }} />
         </Nav.Link>
         <PlacesToGoForm />
       </Container>
       <Container className="pb-5">
-        <Nav.Link as={Link} to={`/PlacesILike`} className="text-warning">
-          Places I Like
+        <Nav.Link as={Link} to="/PlacesILike" className="text-warning">
+          Places I Like {''}
+          <BiHappyHeartEyes size={30} style={{ display: 'inline-block', marginBottom: '5px' }} />
         </Nav.Link>
         <PlacesILikeForm />
       </Container>
       <Container className="pb-5">
-        <Nav.Link as={Link} to={`/PlacesIDontLike`} className="text-warning">
-          Places I Don't Like
+        <Nav.Link as={Link} to="/PlacesIDontLike" className="text-warning">
+          Places I Don't Like {''}
+          <BiSolidDislike size={30} style={{ display: 'inline-block', marginBottom: '5px' }} />
         </Nav.Link>
         <PlacesIDontLikeForm />
       </Container>
